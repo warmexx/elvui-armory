58c76e44060cdbbc36564dbaa55dd1abfdabc949
Maxim Baars <baars@finavista.nl>
2019-12-31 11:44:09 +0100

    Fixes dropdown skinning

--- SkinUtil.lua
+++ SkinUtil.lua
@@ -77,7 +77,7 @@ end
 
 function U.SkinDropDownBox(self, width)
     if ElvUI then
-        s:HandleDropDownBox(self, width, true)
+        s:HandleDropDownBox(self, width)
     else
         s.SkinDropDown(self, width)
     end
